ebd0a17eb8a1558590f06843a9ac35976f587ed1
VladyslavKshenin <legendary.1bk@gmail.com>
2023-10-02 14:40:45 +0300

    update 2 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+container.addEventListener('click', handlerClick);
+
+function createMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => `
+      <li class="gallery__item js-image-item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+      </li>
+    `).join('');
+}
+
+
+function handlerClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+        return;
+   }
+
+  const originalImageURL = event.target.dataset.source;
+  const imageDescription = event.target.getAttribute('alt');
+
+  const instance = basicLightbox.create(`
+      <div class="modal">
+        <img src="${originalImageURL}" alt="${imageDescription}" width="1000" heigth="auto"/>
+      </div>
+    `, {
+    onShow: (instance) => {
+        instance.element().querySelector('img').onclick = instance.close;
+    },
+      
+    onClose: (instance) => {
+    window.removeEventListener('keydown', handlerClose);
+  }
+    });
+  instance.show(); 
+  
+  window.addEventListener('keydown', handlerClose);
+
+function handlerClose(evt) {
+  if (evt.keyCode === 27) {
+    instance.close();
+  };
+  };
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector('.gallery');
 
-console.log(galleryItems);
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+function createMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => `
+  <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
